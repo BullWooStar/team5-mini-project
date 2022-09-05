b0d58031225f7a0fe7c4e75ad541dc18a9222017
Hwa-J <hj1103.code@gmail.com>
2022-09-05 22:56:01 +0900

    refactor: promotion API 연결 - close #18

--- src/store/slices/promotion-product-slice.js
+++ src/store/slices/promotion-product-slice.js
@@ -2,30 +2,32 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const asynPromotionFetch = createAsyncThunk(
-  "promotion/asynPromotionFetch",
+  'promotion/asynPromotionFetch',
   async () => {
-    const response = await axios.get("URL");
-    return response;
+    const response = await axios.get('http://needmoney.ml/products/promotions')
+    return response.data
   }
-);
+)
 
 const PromotionProductSlice = createSlice({
-  name: "promotion",
+  name: 'promotion',
   initialState: {
     products: [],
+    loading: false
   },
   extraReducers: (builder) => {
-    builder.addCase(asynPromotionFetch.pending, () => {
-      console.log("Loading");
-    });
+    builder.addCase(asynPromotionFetch.pending, (state) => {
+      state.loading = true
+    })
     builder.addCase(asynPromotionFetch.fulfilled, (state, { payload }) => {
-      state.products = payload;
-    });
-    builder.addCase(asynPromotionFetch.rejected, () => {
-      console.log("Error");
-    });
-  },
-});
+      state.loading = false
+      state.products = payload
+    })
+    builder.addCase(asynPromotionFetch.rejected, (state) => {
+      state.loading = false
+    })
+  }
+})
 
-export default PromotionProductSlice.reducer;
-export const getPromotionData = (state) => state.promotion;
+export default PromotionProductSlice.reducer
+export const getPromotionData = (state) => state.promotion
