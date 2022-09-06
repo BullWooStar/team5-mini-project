62149eb6385039c1338fd60d18d42ba82eba25c6
BullWooStar <ccatal66@naver.com>
2022-09-06 14:54:10 +0900

    404page 완성, nav bar 수정, 상품검색 후 전체상품으로 돌아갈수 있는 버튼 생성 -#64

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { GiWallet, GiShoppingBag } from "react-icons/gi";
 import { BsCreditCardFill } from "react-icons/bs";
 import * as S from "./style";
-import { useNavigate, NavLink, Route, useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 function NavBar() {
   const navigate = useNavigate();
@@ -14,14 +14,14 @@ function NavBar() {
         {location === "/curation" ? (
           <S.NavButton onClick={() => navigate("/curation")} color="#6B23E0">
             <S.NavIcon color="#6B23E0">
-              <GiWallet size={45} />
+              <GiWallet size={35} />
             </S.NavIcon>
             맞춤추천
           </S.NavButton>
         ) : (
           <S.NavButton onClick={() => navigate("/curation")}>
             <S.NavIcon>
-              <GiWallet size={45} />
+              <GiWallet size={35} />
             </S.NavIcon>
             맞춤추천
           </S.NavButton>
@@ -31,14 +31,14 @@ function NavBar() {
         {location === "/allproduct" ? (
           <S.NavButton onClick={() => navigate("/allproduct")} color="#6B23E0">
             <S.NavIcon color="#6B23E0">
-              <GiShoppingBag size={45} />
+              <GiShoppingBag size={35} />
             </S.NavIcon>
             전체상품
           </S.NavButton>
         ) : (
           <S.NavButton onClick={() => navigate("/allproduct")}>
             <S.NavIcon>
-              <GiShoppingBag size={45} />
+              <GiShoppingBag size={35} />
             </S.NavIcon>
             전체상품
           </S.NavButton>
@@ -48,14 +48,14 @@ function NavBar() {
         {location === "/promotion" ? (
           <S.NavButton onClick={() => navigate("/promotion")} color="#6B23E0">
             <S.NavIcon color="#6B23E0">
-              <BsCreditCardFill size={45} />
+              <BsCreditCardFill size={35} />
             </S.NavIcon>
             프로모션
           </S.NavButton>
         ) : (
           <S.NavButton onClick={() => navigate("/promotion")}>
             <S.NavIcon>
-              <BsCreditCardFill size={45} />
+              <BsCreditCardFill size={35} />
             </S.NavIcon>
             프로모션
           </S.NavButton>
--- src/components/NavBar/style.js
+++ src/components/NavBar/style.js
@@ -1,38 +1,34 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const NavContainer = styled.div`
-    width: 100%;
-    height: 104px;
-    background-color: #FFF;
-    display: inline-flex;
-    align-items: center;
-    justify-content: space-around;
-    border-radius: 20px 20px 0 0;
-    position: fixed;
-    bottom: 0;
-
-`
+  width: 100%;
+  height: 70px;
+  background-color: #fff;
+  display: inline-flex;
+  align-items: center;
+  justify-content: space-around;
+  border-radius: 20px 20px 0 0;
+  position: fixed;
+  bottom: 0;
+`;
 export const NavButton = styled.button`
-    font-size: small;
-    cursor: pointer;
-    align-items: stretch;
-    flex-direction: column;
-    display: flex;
-    outline-color: 0;
-    border: 0;
-    background-color: #fff;
-    color: ${props => props.color};
-    &:hover {
-        color: #6B23E0;
-    }
-
-`
+  font-size: small;
+  cursor: pointer;
+  align-items: stretch;
+  flex-direction: column;
+  display: flex;
+  outline-color: 0;
+  border: 0;
+  background-color: #fff;
+  color: ${(props) => props.color};
+  &:hover {
+    color: #6b23e0;
+  }
+`;
 export const NavIcon = styled.div`
-    margin: auto;
-    color: ${props => props.color};
-    &:hover {
-        color: #6B23E0;
-    }
-
-`
-
+  margin: auto;
+  color: ${(props) => props.color};
+  &:hover {
+    color: #6b23e0;
+  }
+`;
